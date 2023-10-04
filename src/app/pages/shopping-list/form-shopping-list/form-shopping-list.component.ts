48b262a99e4b227a684252f66550293dcc741ffe
danielrsant <danrsant14@gmail.com>
2023-10-03 23:02:21 -0300

    fix id

--- src/app/pages/shopping-list/form-shopping-list/form-shopping-list.component.ts
+++ src/app/pages/shopping-list/form-shopping-list/form-shopping-list.component.ts
@@ -78,6 +78,7 @@ export class FormShoppingListComponent implements OnChanges {
     this.operation = Operation.EDIT;
     this.title = 'Editando #' + this.list.id;
     this.form.enable();
+    this.form.get('id')?.disable();
   }
 
   onSave(): void {
